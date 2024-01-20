a9a97b1d2d3588c9415e8c8e3dcc61c657b5af8a
Rahulchauhan50 <130024449+Rahulchauhan50@users.noreply.github.com>
2024-01-20 09:04:15 +0530

    morning 20-jan

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -7,19 +7,61 @@ export const addMessage = async (req, res, next) => {
         const getUser = onlineUsers.get(to);
         if(message && from && to){
             const newMessage = await prisma.messages.create({
-                data:{
+                data: {
                     message,
-                    sender:{connect:{id:parseInt(from)}},
-                    receiver:{connect:{id:parseInt(to)}},
-                    messageStatus:getUser?"delivered":"sent"
+                    sender: { connect: { id: from } },
+                    reciever: { connect: { id: to } },
+                    messageStatus: getUser ? "delivered" : "sent"
                 },
-                include: {sender:true, receiver: true},
-            })
+                include: { reciever: true, sender: true },  
+            });
             return res.status(201).send({message: newMessage});
         }
         return res.status(400).send("From, to and message is required")
     } catch (error) {
         next(error)   
     }
+}
 
+
+export const getMessages = async (req, res, next) => {
+    try {
+        const prisma = getPrismaInstance();
+        const {from, to } = req.params;
+        const message = await prisma.messages.findMany({
+            where:{
+                OR:[
+                    {
+                        senderId:from,
+                        recieverId: to
+                    },
+                    {
+                        senderId:to,
+                        recieverId: from
+                    },
+                ],
+            },
+            orderBy:{
+                id:"asc"
+            }
+        });
+        const unReadMeassages = [];
+        message.forEach((message, index) => {
+            if(message.messageStatus !== "read" && message.senderId === to){
+                message.messageStatus = "read";
+                unReadMeassages.push(message.id);
+            }
+            
+        });
+
+        await prisma.messages.updateMany({
+            where:{id:{in:unReadMeassages},},
+            data:{messageStatus:"read"}
+        })
+
+        res.status(200).json({message})
+    } catch (error) {
+        next(error)
+        
+    }
 }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import AuthRoutes from './routes/AuthRoutes.js'
 import MessageRoutes from "./routes/MessageRoutes.js"
+import { Server } from 'socket.io'
 
 dotenv.config();
 const port = process.env.PORT || 5000
@@ -14,8 +15,32 @@ app.use(express.json())
 app.use("/api/auth",AuthRoutes)
 app.use("/api/messages",MessageRoutes)
 
-app.listen(port,()=>{
+
+const server = app.listen(port,()=>{
     console.log(`you are listening at ${port}`)
 })
 
-global.onlineUsers = new Map()
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:3000",
+    },
+})
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket)=>{
+    global.chatShocket = socket;
+    socket.on("add-user", (userId)=>{
+        onlineUsers.set(userId, socket.id);
+    })
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", {
+                from: data.from,
+                message: data.message,
+            });
+        }
+        onlineUsers.set(data.to, socket.id);
+    })
+})
